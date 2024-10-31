0d8e83410d76ab059360d40cf8f90f6b47a0e00f
zhanglb <zhanglb21@mails.tsinghua.edu.cn>
2024-10-31 21:09:01 +0800

    init commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Welcome(View view){
+        Toast.makeText(this, "欢迎来到安卓世界", Toast.LENGTH_SHORT).show();
+    }
+}
